d4fb7781270a1e43abf9d943f836b047b9d6044c
AminesCodes <aminebensalem@pursuit.org>
2020-01-21 23:49:21 -0500

    Cleared a function that was not used

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ export default function Home(props) {
       pageContent = props.searchResult.map(result => 
         <div className='col-sm-6'>
             <Link className='col-sm-6' to={`/video/${result.id.videoId}`} key={result.id.videoId+result.snippet.thumbnails.medium.url}>
-            <VideoThumbnail onClick={e => props.handleVideoSelection(result.id.videoId)}
+            <VideoThumbnail 
                 url={result.snippet.thumbnails.medium.url}
                 title={result.snippet.title}
                 id={result.id.videoId}
